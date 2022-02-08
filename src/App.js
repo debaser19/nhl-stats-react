62d50b868b38297bbb43fccb81f427d24206950a
Steve Stoveld <steve@stoveld.com>
2022-02-07 21:55:20 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Header from './components/Header'
+import Header from './components/Header';
 import GameScores from './components/scores/GameScores';
 import Stats from './components/stats/Stats';
 import Standings from './components/standings/Standings';
-import Footer from './components/Footer'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-    	<Router>
-    		<Header />
-    		<Routes>
-				<Route path='/' element={<GameScores />} />
-				<Route path='/stats' element={<Stats />} />
-				<Route path='/standings' element={<Standings />} />
-        	</Routes>
-    	</Router>
-    	<Footer />
+    		<Router>
+        		<Header />
+        		<Routes>
+					<Route path='/' element={<GameScores />} />
+					<Route path='/stats' element={<Stats />} />
+					<Route path='/standings' element={<Standings />} />
+        		</Routes>
+      		</Router>
+      		<Footer />
     	</div>
 	);
 };
